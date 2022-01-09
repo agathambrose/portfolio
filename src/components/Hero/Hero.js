9ac383e64747a6f40667ce450c37a6b86dcb0608
Agatha Ambrose <agathambrose@gmail.com>
2022-01-09 13:30:01 +0000

    Updated the Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,45 +4,47 @@ import Typical from "react-typical";
 
 export const Hero = () => {
   return (
-    <section className="hero-div">
-      <div className="px-3 py-2 mt-5 hero-info">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontFamily: "Poppins, sans-serif",
-          }}
-        >
-          <h2
-            style={{
-              fontWeight: "500",
-            }}
-            className='name-div'
-          >
-            Agatha Ambrose
-          </h2>
-        </div>
-        <p
-          style={{
-            fontFamily: "Poppins, sans-serif",
-          }}
-          className='typical-div'
-        >
-          <Typical
-            steps={[
-              "A Frontend Dev",
-              800,
-              "A Writer",
-              800,
-              "An Accountant",
-              800,
-            ]}
-            loop={Infinity}
-            wrapper="p"
-          />
-        </p>
-      </div>
-    </section>
-  );
+		<section className="hero-div">
+			<div className="px-3 py-2 mt-5 hero-info">
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						fontFamily: "Poppins, sans-serif",
+					}}
+				>
+					<h2
+						style={{
+							fontWeight: "500",
+						}}
+						className="name-div"
+					>
+						Agatha Ambrose
+					</h2>
+				</div>
+				<p
+					style={{
+						fontFamily: "Poppins, sans-serif",
+					}}
+					className="typical-div"
+				>
+					<Typical
+						steps={[
+							"A Product Manager",
+							800,
+							"A Frontend Dev",
+							800,
+							"A Creative Writer",
+							800,
+							"An Ex-Accountant",
+							800,
+						]}
+						loop={Infinity}
+						wrapper="p"
+					/>
+				</p>
+			</div>
+		</section>
+	);
 };
